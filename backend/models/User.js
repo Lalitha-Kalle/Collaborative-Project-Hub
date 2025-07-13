6c821afaf08714355846b78e535fd0d3308fc432
Lalitha-Kalle <lalithaprogramming@gmail.com>
2025-07-13 17:28:08 +0530

    Modified User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,36 @@
 const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  password: Number, 
-  role: String, 
-  bio: String,
-})
+  name: {
+    type: String,
+    required: [true, "User Name is required"],
+    trim: true,
+    minLength: 3,
+    maxLength: 50
+  },
+  email: {
+    type: String,
+    required: [true, "User name is required"],
+    trim: true,
+    lowercase: true,
+    match: [/\S+@\S+\.\S/, "Please enter a valid email address (must include '@' and a domain, e.g., user@example.com)"],
+  },
+  password: {
+    type: Number,
+    required: [true, "User Password is required"],
+    minLength: 6
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user'
+  }, 
+  bio: {
+    type: String,
+    maxLength: 50
+  }
+}, { timestamps: true})
 
 const User = mongoose.model("User", userSchema)
-console.log(User)
+
 module.exports = User
